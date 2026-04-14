976a07ef01715eb5389e94f54a804f7c44ffffff
Ali <afakhreddine717@gmail.com>
2026-04-14 13:08:23 +0300

    updates

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -254,6 +254,7 @@ export const Hero = () => {
         const handleVisibilityChange = () => {
             if (!document.hidden) {
                 attemptPlay();
+                
             }
         };
 
