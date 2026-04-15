1220162fe98afaf462e1923bac1bda3e5dba81ac
Ali <afakhreddine717@gmail.com>
2026-04-15 12:52:06 +0300

    updating the projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,151 +7,131 @@ export const projects = [
         style: "Minimalist Style"
     },
     {
-        name: 'Mr. Enoch Ose',
+        name: 'Dome pillar',
         folder: 'Project-2',
-        imageCount: 8,
-        category: 'Interior Design',
-        style: "Modern Style"
+        imageCount: 26,
+        category: 'Full Design',
+        style: "Minimalist Style"
     },
     {
-        name: 'Madame Fawzia Mohammed',
+        name: 'Project 3',
         folder: 'Project-3',
-        imageCount: 8,
-        category: 'Interior Design',
-        style: "Contemporary Style"
-    },
-    {
-        name: 'Madame Serwa Asentewa',
-        folder: 'Project-4',
-        imageCount: 9,
-        category: 'Interior Design',
-        style: "Tropical Modern Style"
-    },
-    {
-        name: 'Project 5',
-        folder: 'Project-5',
         imageCount: 4,
         category: 'Interior Design',
         style: "Classic Style"
     },
     {
-        name: 'Project 6',
-        folder: 'Project-6',
+        name: 'Project 4',
+        folder: 'Project-4',
         imageCount: 3,
         category: 'Architectural Design',
         style: "Classic Style Villa"
     },
     {
-        name: 'Project 7',
-        folder: 'Project-7',
+        name: 'Project 5',
+        folder: 'Project-5',
         imageCount: 10,
         category: 'Interior Design',
         style: "Contemporary Style"
     },
     {
         name: 'Mr. Ali Ganza',
-        folder: 'Project-8',
+        folder: 'Project-6',
         imageCount: 5,
         category: 'Interior Design',
         style: "Modern Style Studio"
     },
     {
         name: 'Mr. Kwame',
-        folder: 'Project-9',
+        folder: 'Project-7',
         imageCount: 2,
         category: 'Interior Design',
         style: "Contemporary Modern Kitchen"
     },
     {
-        name: 'Project 10',
-        folder: 'Project-10',
+        name: 'Project 8',
+        folder: 'Project-8',
         imageCount: 7,
         category: 'Interior Design',
         style: "Minimalist Colourful Style"
     },
     {
         name: 'Madame Ama Gifty badu',
-        folder: 'Project-11',
+        folder: 'Project-9',
         imageCount: 3,
         category: 'Architectural Design',
         style: "Contemporary Style"
     },
     {
-        name: 'Project 12',
-        folder: 'Project-12',
+        name: 'Project 10',
+        folder: 'Project-10',
         imageCount: 9,
         category: 'Interior Design',
         style: "Tropical Contemporary Style"
     },
     {
         name: 'Madame Vanessa',
-        folder: 'Project-13',
+        folder: 'Project-11',
         imageCount: 10,
         category: 'Full Design',
         style: "Industrial Style"
     },
-    {
-        name: 'Project 14',
-        folder: 'Project-14',
-        imageCount: 4,
-        category: 'Interior Design',
-        style: "Contemporary Style"
-    },
+
     {
         name: 'Mr. Boakye Kensah',
-        folder: 'Project-15',
+        folder: 'Project-12',
         imageCount: 10,
         category: 'Full Design',
-        style: "Project-15"
+        style: "Project-12"
     },
     {
         name: 'Alhaji Habib',
-        folder: 'Project-16',
+        folder: 'Project-13',
         imageCount: 4,
         category: 'Architectural Design',
         style: "Contemporary Minimalism"
     },
     {
         name: 'Madame Bernice',
-        folder: 'Project-17',
+        folder: 'Project-14',
         imageCount: 2,
         category: 'Architectural Design',
         style: "Neo-Brutalist Modernism Style"
     },
     {
-        name: 'Project-18',
-        folder: 'Project-18',
+        name: 'Project-15',
+        folder: 'Project-15',
         imageCount: 3,
         category: 'Architectural Design',
         style: "Afro-Minimalist Luxury Modernism"
     },
     {
         name: 'Madame Cynthia Serwaa',
-        folder: 'Project-19',
+        folder: 'Project-16',
         imageCount: 4,
         category: 'Architectural Design',
         style: "Contemporary Tropical Minimalism"
     },
     {
         name: 'Mr. Kweku Acheampong',
-        folder: 'Project-20',
+        folder: 'Project-17',
         imageCount: 2,
         category: 'Architectural Design',
         style: "Tropical Modern Luxury style"
     },
     {
-        name: 'Project-21',
-        folder: 'Project-21',
+        name: 'Project-18',
+        folder: 'Project-18',
         imageCount: 4,
         category: 'Architectural Design',
         style: "West African local style"
     },
     {
-        name: 'Project-22',
-        folder: 'Project-22',
+        name: 'Project-19',
+        folder: 'Project-19',
         imageCount: 19,
         category: 'Interior Design',
-        style: "Kitchen Design"
+        style: "Project-19"
     },
 
 ];
